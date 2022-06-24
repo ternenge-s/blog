717ba82285a0f63d5ad138b0dcd417f40f5f279d
ternenge-s <tekpriest.org@gmail.com>
2022-06-24 16:53:42 +0100

    updated

--- server/server.js
+++ server/server.js
@@ -74,6 +74,8 @@ app.use(express.json());
 
 app.use('/', router);
 
-app.listen(3001, () => {
-  console.log('listening on localhost:3001');
+app.listen(process.env.PORT || 3001, (err) => {
+	if (err) console.log(err);
+
+	console.log('server started')
 });
