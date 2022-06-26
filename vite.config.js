a4f336adf783dc93d224e9add8e8181e83651012
ternenge-s <tekpriest.org@gmail.com>
2022-06-26 14:56:04 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
-function getManualChunk(dependencies) {}
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    chunkSplitPlugin({
-      strategy: 'single-vendor',
-      customSplitting: {
-        'react-vendor': ['react', 'react-dom'],
-        'icon-vendor': ['react-icons'],
-        'assets-vendor': ['/src/assets/'],
-        'tailwind-vendor': ['tailwindcss'],
-      },
-    }),
-  ],
+
+export default defineConfig(({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return {
+    /* define: {
+      SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+      SANITY_ACCESS_TOKEN: process.env.SANITY_ACCESS_TOKEN,
+      PORT: process.env.PORT,
+      MAIL_PASS_KEY: process.env.MAIL_PASS_KEY,
+    },*/
+    plugins: [
+      react(),
+      chunkSplitPlugin({
+        strategy: 'single-vendor',
+        customSplitting: {
+          'react-vendor': ['react', 'react-dom'],
+          'icon-vendor': ['react-icons'],
+          'assets-vendor': ['/src/assets/'],
+          'tailwind-vendor': ['tailwindcss'],
+        },
+      }),
+    ],
+  };
 });
